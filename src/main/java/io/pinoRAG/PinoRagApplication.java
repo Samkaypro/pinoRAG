9d6fc74fd317394543112336e5658765fd14486b
Samkaypro <samkaypro@gmail.com>
2026-06-03 21:33:09 +0100

    init pinoRAG

---
+++ src/main/java/io/pinoRAG/PinoRagApplication.java
@@ -0,0 +1,13 @@
+package io.pinoRAG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinoRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinoRagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/pinoRAG/PinoRagApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pinoRAG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinoRagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
